f4381c88284d4ab48080963ac2259aa27b76b059
dav-id-b <105126213+dav-id-b@users.noreply.github.com>
2022-05-13 00:30:44 -0400

    Update numbers larger than 25 loop.py

--- numbers larger than 25 loop.py
+++ numbers larger than 25 loop.py
@@ -1,4 +1,4 @@
-#This code will determine which numbers in an array are greater than 25
+#This loop will determine which numbers in an array are greater than 25
 
 
 for num in [8, 21, 14, 90, 16, 1, 4, 2, 7, 3, 99, 102, 5]:
