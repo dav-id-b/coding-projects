ee1282128896683bf3753d801b61664eabe152f8
dav-id-b <105126213+dav-id-b@users.noreply.github.com>
2022-05-13 00:30:21 -0400

    Update numbers larger than 25 loop.py

--- numbers larger than 25 loop.py
+++ numbers larger than 25 loop.py
@@ -1,9 +1,7 @@
-#This code will determine the int values in a list that are greater than 25
+#This code will determine which numbers in an array are greater than 25
 
-print('Beginning')
 
 for num in [8, 21, 14, 90, 16, 1, 4, 2, 7, 3, 99, 102, 5]:
     if num > 25:
         print('Larger than 25:', num)
         
-print('End')
