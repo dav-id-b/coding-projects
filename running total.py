878bd140b8ffd1fbe590f13b3c64c32756c9737b
dav-id-b <105126213+dav-id-b@users.noreply.github.com>
2022-05-17 23:31:33 -0400

    Update running total.py

--- running total.py
+++ running total.py
@@ -5,7 +5,7 @@ total = 0
 print('Beginning', total)
 
 for num in [2, 1, 7, 5, 12, 16, 99, 69, 4, 20, 66,]:
-    total = total + num
+    total += num
     print(total, num)
     
 print('End', total)
