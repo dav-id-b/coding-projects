e68e75162e1fc793978eaed499c6ed5859f49515
dav-id-b <105126213+dav-id-b@users.noreply.github.com>
2023-05-12 20:15:39 -0400

    Add files via upload

---
+++ coding-projects-main/python-code/search specified array indices for int value.py
@@ -0,0 +1,13 @@
+#This function will determine if a specific int value is found within the first 5 indices of a collection of numbers
+
+#9 in the if statement can be swapped out for another value to be searched for
+
+#the range of indices checked can be changed by altering the values specificed after nums in the if statement 
+
+nums =
+
+def within_array(nums):
+  if 9 in nums[0:5]:
+    return True
+  else:
+    return False
